3d1ff7809b3bc17950aa0fe7f5168d3988e78643
Leticia <leticia.aragao00@gmail.com>
2024-04-25 19:57:43 +0200

    completed dark mode

---
+++ script.js
@@ -0,0 +1,9 @@
+// document.querySelector('#btn').addEventListener('click', () => {
+//     document.body.classList.toggle('dark-mode');
+// })
+
+const buttonDarkMode = document.querySelector('#btn');
+
+buttonDarkMode.addEventListener('click', () =>{
+    document.body.classList.toggle('dark-mode');
+})
